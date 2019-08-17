11f00b8bdda4d690faaadd19ee8ff321c179a623
Carolyn Hicks <carolyn@bannoy.net>
2019-08-17 22:15:08 +1000

    reinstate run-server mode for local dev

--- bin/run-server.sh
+++ bin/run-server.sh
@@ -6,18 +6,23 @@ then
 fi
 if [[ -n "$HTTP_HOST" ]]
 then
-    echo "got http hosts: $HTTP_HOST"
     SERVER_NAMES=$(echo $HTTP_HOST | sed -e "s#,# #g")
     sed -i "s/HTTP_HOST/$SERVER_NAMES/" /etc/nginx/sites-enabled/cegov_nginx.conf
 fi
 
-service nginx start
-uwsgi --ini /etc/uwsgi.ini
+if [[ "$RUN_DEBUG_MODE" == "True" ]]
+then
+    service nginx stop
+    python manage.py runserver  --insecure 0.0.0.0:8000
+else
+    service nginx start
+    uwsgi --ini /etc/uwsgi.ini
 
-# this is awful but somehow the logs get created as root when uwsgi starts
-# and are not writable by www-data otherwise
-chown www-data /var/log/django/*log
+    # this is awful but somehow the logs get created as root when uwsgi starts
+    # and are not writable by www-data otherwise
+    chown www-data /var/log/django/*log
 
-tail -f /var/log/django/request.log
+    tail -f /var/log/django/request.log
+fi
 
 exit 0
