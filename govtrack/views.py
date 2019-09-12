492e18a4a953d94687a8f529ad4888f737d24cc4
Joeyrsp <joeysuttonpreece@gmail.com>
2019-09-12 14:57:26 +1000

    reverse inbox sort order

--- govtrack/views.py
+++ govtrack/views.py
@@ -36,7 +36,7 @@ def area(request, area_id):
     area = get_object_or_404(Area, pk=area_id)
     records = area.build_hierarchy()
 
-    import_declarations = ImportDeclaration.objects.filter(country=area.country).order_by('-date')
+    import_declarations = ImportDeclaration.objects.filter(country=area.country).order_by('date')
 
     return render(request, 'govtrack/area.html', {
         'area': area,
@@ -83,7 +83,7 @@ def country(request, country_id, action='view'):
     structure = country.get_root_structure().build_hierarchy()
     records = country.get_root_area().build_hierarchy()
 
-    import_declarations = ImportDeclaration.objects.filter(country=country).order_by('-date')
+    import_declarations = ImportDeclaration.objects.filter(country=country).order_by('date')
 
     total_pop = 0
     item_seen = set()
@@ -169,7 +169,7 @@ def structure_child(request, parent_id):
 def area_edit(request, area_id):
     area = get_object_or_404(Area, pk=area_id)
     form = AreaForm(instance=area)
-    import_declarations = ImportDeclaration.objects.filter(country=area.country).order_by('-date')
+    import_declarations = ImportDeclaration.objects.filter(country=area.country).order_by('date')
 
     link_initial = {
         'content_type': Area.content_type_id(),
@@ -287,7 +287,7 @@ def declaration_edit(request, declaration_id):
     if not dec:
         raise Http404("No such declaration")
     form = DeclarationForm(instance=dec)
-    import_declarations = ImportDeclaration.objects.filter(country=dec.area.country).order_by('-date')
+    import_declarations = ImportDeclaration.objects.filter(country=dec.area.country).order_by('date')
     link_initial = {
         'content_type': Declaration.content_type_id(),
         'object_id': declaration_id,
